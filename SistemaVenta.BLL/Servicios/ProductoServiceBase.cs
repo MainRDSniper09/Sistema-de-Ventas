5fefb3c04bd3d2453fe123ae8b13061580542675
Juan Nicolas Barreto Sanchez <barretosanchez6@gmail.com>
2024-09-15 13:17:37 -0500

    Backend
    Back end finalizado con C# ASP.NET

---
+++ SistemaVenta.BLL/Servicios/ProductoServiceBase.cs
@@ -0,0 +1,28 @@
+﻿namespace SistemaVenta.BLL.Servicios
+{
+    public class ProductoServiceBase
+    {
+
+        public async Task<bool> Eliminar(int id)
+        {
+            try
+            {
+                var productoEncontrado = await _productoRepositorio.Obtener(p => p.IdProducto == id);
+
+                if (productoEncontrado == null)
+                    throw new TaskCanceledException("El producto no existe");
+
+                bool respuesta = await _productoRepositorio.Eliminar(productoEncontrado);
+
+                if (!respuesta)
+                    throw new TaskCanceledException("No se pudo eliminar");
+
+                return respuesta;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ SistemaVenta.BLL/Servicios/UsuarioServiceBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class UsuarioServiceBase
+    {
+
+        public async Task<SesionDTO> ValidarCredenciales(string correo, string clave)
+        {
+            try
+            {
+                var queryUsuario = await _usuarioRepositorio.Consultar(u => u.Correo == correo && u.Clave == clave);
+
+                if (queryUsuario.FirstOrDefault() == null)
+                {
+                    throw new TaskCanceledException("El usuario no existe");
+
+                    Usuario devolverUsuario = queryUsuario.Include(rol => rol.IdRolNavigation).First();
+                    return _mapper.Map<SesionDTO>(devolverUsuario);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
